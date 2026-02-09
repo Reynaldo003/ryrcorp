db83db123f557ad170b7373f9843a7d454590a31
Reynaldo003 <rvallejo276@gmail.com>
2026-02-09 17:18:35 -0600

    conexion server

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: ["1729-2806-10a6-6-2142-49ed-5b4d-d374-ac68.ngrok-free.app"],
+    allowedHosts: [
+      "1729-2806-10a6-6-2142-49ed-5b4d-d374-ac68.ngrok-free.app",
+      "ryrcorp.vercel.app",
+    ],
   },
 });
